80758d492f495154d9078a5ae1b8430da042b34a
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-06-22 17:27:03 +0900

    repeat1成功

---
+++ src/answer/repeat/repeat1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>repeat1</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // for文を用いて、51から100までの数字を
+    // echoしてください。echoするときに改行も入れるようにしてください。
+    for ($i=51; $i <= 100; $i++) { 
+        echo $i.'<br>';
+    }
+  ?>
+
+</body>
+</html>
