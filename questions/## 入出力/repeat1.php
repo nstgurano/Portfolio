125211c01a298e09e0639e344eda9306ea672d64
unknown <ijichi@nstg.co.jp>
2020-06-22 16:16:56 +0900

    ## 入出力 繰り返し

---
+++ questions/## 入出力/repeat1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>repeat1</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // for文を用いて、51から100までの数字を
+    // echoしてください。echoするときに改行も入れるようにしてください。
+    
+  ?>
+
+</body>
+</html>
---
+++ questions/## 入出力/repeat3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>repeat3</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // for文を用いて1から1000までの数字をechoしてください。
+    // ただし、501以上の数字を表示しないようにbreak文を用いてfor文を抜けてください。
+    // echoするときに改行も入れるようにしてください。
+        
+  ?>
+
+</body>
+</html>
