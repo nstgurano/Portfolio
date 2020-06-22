0b3250886b10f12d4d12c2c3710c8079185be493
unknown <ijichi@nstg.co.jp>
2020-06-22 15:42:18 +0900

    ## 入出力 繰り返し　問題

---
+++ questions/## 入出力/繰り返し/repeat1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>repeat1</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // for文を用いて、51から100までの数字をechoしてください。
+    // echoするときに改行も入れるようにしてください。
+    
+  ?>
+
+</body>
+</html>
---
+++ questions/## 入出力/繰り返し/repeat5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>repeat5</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+        $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+    // 連想配列$scoresのキーを変数$key、値を変数$valueとするforeach文を書いてください。
+    // foreach文を使用して各教科の点数が、「数学は70点です。」のように出力されるようechoしてください。
+    
+  ?>
+
+</body>
+</html>
