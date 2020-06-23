bfba39c650f91d2188b1c84e2776955d38cf16c4
unknown <ijichi@nstg.co.jp>
2020-06-23 10:34:10 +0900

    入出力　配列（入力/出力）

---
+++ src/questions/array/arrayIO.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>arrayIO</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $colorsという配列を作り、赤、青、黄の順番に入れてください。
+
+    // $colorsの最初の要素をechoしてください。
+
+    // $colorsの末尾に白を追加し、$colorsの最後の要素をechoしてください。
+
+  ?>
+
+</body>
+</html>
---
+++ src/questions/array/arraySearch.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>arraySearch</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $employeesという配列に社員名が入っています。
+    $employees = array("佐藤","鈴木","高橋","田中","伊藤","渡辺","山本","中村","小林","加藤");
+    // $employeesと$employeeNameを引数に取り、$employeesに$employeeNameがいれば
+    // ex: 弊社に田中はおります。
+    // いなければ
+    // ex: 弊社に亀田はおりません。
+    // とechoするメソッド　searchEmployee($employees, $employeeName)　を作成して
+    // いる場合と、いない場合、両方を実行してください。
+
+  ?>
+
+</body>
+</html>
