03d17924d9a3fd63eb433d29ff88c300dda125a1
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-06-26 13:10:24 +0900

    primenumber　成功

---
+++ src/answer/math/primenumber.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>primenumber</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // 1から100までの素数をすべて書き出し、「１から100までの素数は◯個あります。」と表示してください。
+    // 結果の素数と素数との間はスペースを入れてください。
+    $array=[];
+
+    for ($i=2; $i <=100 ; $i++) { 
+      $k=true;
+      for ($j=2; $j <$i ; $j++) { 
+        if ($i===2) {
+          $array[]=2;
+        }elseif($i%$j==0) {
+          $k=false;
+        }
+
+      }
+      if ($k===true) {
+        $array[]=$i;
+      }
+    }
+    echo '1~100までの素数は'.count($array).'個あります。';
+    foreach ($array as $key) {
+      echo $key.'&nbsp';
+    }
+    ?>
+
+</body>
+</html>
