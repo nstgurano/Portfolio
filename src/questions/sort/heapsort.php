3429fc7faa7b9b06f0feff35f4e1b5857aaefc13
unknown <ijichi@nstg.co.jp>
2020-06-26 09:50:14 +0900

    ヒープソート

---
+++ src/questions/sort/heapsort.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>heapsort</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+	// ヒープソートを使用して配列$numbersの値を小さい順番に並べて下さい。
+	//            70
+	//          ／   ＼
+	//        28       72
+	//     ／   ＼   ／   ＼
+	//    93     98 41     68
+	// ／
+	//26
+	// また上のように配列の値の交換が行われるたびに、配列の値を木形式で表示してください。
+	$nums = array(70,28,68,93,98,41,72,26);
+	
+  ?>
+
+</body>
+</html>
