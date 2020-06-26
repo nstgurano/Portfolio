ad3dc65a705f701f6657d0267c91679863fc0fbb
unknown <ijichi@nstg.co.jp>
2020-06-26 14:20:24 +0900

    再帰 ハノイの塔

---
+++ src/questions/recursion/Hanoi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Hanoi</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+  // $n　移動させる円盤の枚数
+  // $from　移動元の棒の名前
+  // $work　作業用に使う棒の名前
+  // $dest　移動先の棒の名前
+  // 上の4個を引数とする、ハノイの塔を実現するメソッドHanoiを作成してください。
+  // 出力例: Hanoi(3,"A","B","C");
+  // Move the disc from A to C.
+  // Move the disc from A to B.
+  // Move the disc from C to B.
+  // Move the disc from A to C.
+  // Move the disc from B to A.
+  // Move the disc from B to C.
+  // Move the disc from A to C.
+
+function Hanoi($n, $from, $work, $dest) {
+
+}
+Hanoi(3,"A","B","C");
+
+  ?>
+
+</body>
+</html>
