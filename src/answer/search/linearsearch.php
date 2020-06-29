3639408d337dce5185b2dc06326a938f3f4b043b
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-06-29 13:31:16 +0900

    linearSearch 成功

---
+++ src/answer/search/linearsearch.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>linearsearch</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $aに数字の配列が格納されています。探索値を$kyに代入します。
+    // 配列$a から$kyと一致する全要素のインデックスを、
+    // 配列$indexの先頭から順に格納し、その$indexをechoして、
+    // 一致した要素数を返す以下のメソッド searchIdxを作成して下さい。
+    // また最後に要素数を あればechoして下さい。
+    // ex $ky = 9 の時
+    // その値は[1]にあります。その値は[3]にあります。その値は[7]にあります。
+    // 3個ありました。
+    // ex2 $ky = 100の時
+    // その値はないです。
+
+    $a = array(1,9,2,9,4,6,7,9);
+    $ky = 100;
+
+    searchIdx($a, $ky,$index);
+
+    function searchIdx($a,$ky)
+    {
+      $b=count($a);//
+      $index=[];
+
+      for ($i=0; $i <$b ; $i++) { //$indexの配列を$aの要素だけ用意
+        for ($i=0; $i < $b; $i++) { //$aの要素を0~$b-1だけチェック
+          if ($ky!==$a[$i]) {//$aの配列の中に$kyがあれば$indexに挿入,
+            continue;
+          }else{
+            $index[]=$i;
+          }
+        }
+      }
+
+    if(empty($index)) {
+        echo 'その値はないです。';
+    }else {
+      foreach ($index as $key) {
+        echo 'その値は['.$key.']にあります。';
+      }
+    }
+   }
+    
+    
+?>
+
+</body>
+</html>
