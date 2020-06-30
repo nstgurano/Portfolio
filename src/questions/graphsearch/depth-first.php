dd08382c07d320e79e4a95db69552ad0a9021b50
unknown <ijichi@nstg.co.jp>
2020-06-30 14:55:47 +0900

    グラフ探索 深さ優先探索

---
+++ src/questions/graphsearch/depth-first.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>depth-first</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+/*
+ 
+  0 X S 0 X 0
+  0 G X 0 0 0
+  X 0 0 X 0 0
+  X X 0 0 0 X
+  
+  
+　のような迷路データをcsvから読み込む等して、自分で作成してください。
+
+  注）0は進め、Xは障害物で進めないとします。　S 出発点 G ゴールを表します。
+
+  深さ優先探索を使って、ゴールできた場合
+  0 X S 1 X 0
+  0 G X 1 1 1
+  X 1 1 X 1 1
+  X X 1 1 1 X
+  
+  上のように、迷路データの通ってきた道を 0 → 1 へと変換して出力してください。
+ */
+
+  ?>
+
+</body>
+</html>
