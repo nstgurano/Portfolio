65799ecd6f31d4225c17701392fd60de5214ea97
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-01 09:26:20 +0900

    途中

---
+++ src/answer/recursion/Hanoi.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Hanoi</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+  // $n　移動させる円盤の枚数
+  // $from　移動元の棒の名前
+  // $work　作業用に使う棒の名前
+  // $dest　移動先の棒の名前
+  // 上の4個を引数とする、ハノイの塔を実現するメソッドHanoiを作成してください。
+  // 出力例: Hanoi(3,"A","B","C");
+  // Move the disc from A to C.
+  // Move the disc from A to B.
+  // Move the disc from C to B.
+  // Move the disc from A to C.
+  // Move the disc from B to A.
+  // Move the disc from B to C.
+  // Move the disc from A to C.
+
+Hanoi(3,"A","B","C");
+
+function Hanoi($n, $from, $work, $dest) {//上記のコメントアウトの通りの変数設定
+  if ($n>0) {//移動させる円盤の数は0以上である
+   // Hanoi($n-1,$from,$dest,$work);//上からn-1番目の円盤を使う、AからCへ移す
+    echo 'Move to the disk '.$n.' from '.$from.' to '.$dest;
+    echo '<br>';
+    Hanoi($n-1,$work,$from,$dest);//上からn-1番目の円盤をBからへA移す
+  }
+}
+
+  ?>
+
+</body>
+</html>
