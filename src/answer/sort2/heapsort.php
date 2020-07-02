742a9545a9560decd161c88e1ef5a9ddcb46900b
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-02 17:36:07 +0900

    ほぼ完成

---
+++ src/answer/sort2/heapsort.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>heapsort</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+	// ヒープソートを使用して配列$numsの値を小さい順番に並べて下さい。
+	//            70
+	//          ／   ＼
+	//        28       72
+	//     ／   ＼   ／   ＼
+	//    93     98 41     68
+	// ／
+	//26
+	// また上のように配列の値の交換が行われるたびに、配列の値を木形式で表示してください。
+	$nums = array(70,28,68,93,98,41,72,26);
+	$arr=[];
+	$countNums=count($nums)-1;//$numsの配列の長さ確認
+
+	echo '【初期値】'.'<br>';
+	foreach ($nums as $key) {
+		echo $key.'&nbsp';
+	}
+
+	heap($arr,$nums,$countNums);//引数は、ソートしたものを入れる配列、ソートする配列、ソートする配列の長さ
+
+
+	echo '【ソート後】'.'<br>';
+	foreach ($arr as $key) {
+		echo $key.'&nbsp';
+	}
+
+	function echo_tree(&$arr){
+		global $br;
+		$br='<br>';
+        $string="";
+        $string.="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$arr[0]}".$br;
+        $string.="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;／&nbsp;&nbsp;&nbsp;＼".$br;
+        $string.="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$arr[1]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$arr[2]}".$br;
+        $string.="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;／&nbsp;&nbsp;&nbsp;＼&nbsp;&nbsp;&nbsp;／&nbsp;&nbsp;&nbsp;＼".$br;
+        $string.="&nbsp;&nbsp;&nbsp;&nbsp;{$arr[3]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$arr[4]}&nbsp;{$arr[5]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$array[6]}".$br;
+        $string.="&nbsp;&nbsp;／".$br;
+        $string.="{$arr[7]}".$br.$br;
+        return $string;
+	 }
+
+	
+	function heap(&$arr,$nums,$countNums)//$arrは参照渡し
+	{
+		for ($i=$countNums; 0<=$i; $i--) {//配列の最後の要素から始める
+			$parent=floor(($i-1)/2);//葉もしくは枝要素の親の値
+
+			if ($nums[$i]<$nums[$parent]) {//葉もしくは枝要素が親よりも小さい場合
+				echo_tree($arr);
+				echo '<br>'.$nums[$i].'と'.$nums[$parent].'を入れ替え'.'<br>';
+				$tmp=$nums[$parent];
+				$nums[$parent]=$nums[$i];
+				$nums[$i]=$tmp;
+			}
+		}
+
+		$arr[]=array_shift($nums);//$numsの中の始めの要素を取り出す
+		$countNums -=1;//始めの要素を取り出し一つ要素が少なくなったので、－１
+
+		if ($countNums>=0) {//要素を０まで調べ終わったら再起処理
+			heap($arr,$nums,$countNums);//
+		}
+	}
+	
+
+  ?>
+
+</body>
+</html>
