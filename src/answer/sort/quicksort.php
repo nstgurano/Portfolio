ab2b7033c85a5be76e4c12c1be7299a26a3c054b
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-06 14:01:48 +0900

    途中

--- src/answer/sort/quicksort.php
+++ src/answer/sort/quicksort.php
@@ -8,8 +8,8 @@
 <body>
 
   <?php
-    $a = array(10,3,1,9,7,6,8,2,4,5);
-    $b=count($a);
+    $a = [10,3,11,1,9,7,6,8,2,4,5];
+    $a_last=count($a)-1;
   // クイックソートを使用して配列$aの値を小さい順番に並べて下さい。
   // 10 3 1 9 7 6 8 2 4 5
   // 範囲:0-9 基準値: 7 [0]と[9]を入れ替え
@@ -23,16 +23,14 @@
     echo $key.'&nbsp';
   }
   echo '<br>';
-  quickSort($a,0,$b-1);//引数は配列a、配列の最初の要素、配列の最後の要素
+  quickSort($a,0,$a_last);//引数は配列a、配列の最初の要素、配列の最後の要素
 
   function quickSort(&$a,$first,$last)//
   {
-
-  while ($first<=$last) {//
     $firstpoint=$first;//左から始まる要素
     $lastpoint=$last;//右から始まる要素
     $center=ceil(($firstpoint+$lastpoint)/2);//真ん中の要素（切り捨て）
-
+  while ($firstpoint<=$lastpoint) {//
     while ($a[$firstpoint]<$a[$center]) {//真ん中と左を比べて、左が小さいなら一つ右に行く
       $firstpoint++;
     }
@@ -54,8 +52,6 @@
       $firstpoint++;//次の要素を確認するために左と右どちらも進む
       $lastpoint--;
     }
-
-
     echo '<br>';
     foreach ($a as $key) {
       echo $key.'&nbsp';
@@ -65,10 +61,10 @@
     echo '<br>';
 
     if ($first<$lastpoint) {//真ん中より左の要素にソートできるものがある場合、再起処理
-      quickSort($a, $first , $lastpoint);//引数は若干ソートされたa、左端の要素、真ん中の要素
+      quickSort($left, $first , $lastpoint);//引数は若干ソートされたa、左端の要素、真ん中の要素
     }
     if ($firstpoint<$last) {//真ん中より右の要素にソートできるものがある場合、再起処理
-      quickSort($a, $firstpoint,$last);//引数は若干ソートされたa、真ん中の要素、右端の要素
+      quickSort($right, $firstpoint,$last);//引数は若干ソートされたa、真ん中の要素、右端の要素
     }
   }
 
