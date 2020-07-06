c255a6db2cce687d4b0577a1468249b678c530a2
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-06 12:36:04 +0900

    meargesort　成功

--- src/answer/sort2/mergesort.php
+++ src/answer/sort2/mergesort.php
@@ -46,7 +46,8 @@
 	// 配列のマージが終了しました。
 	// Array ( [0] => 1 [1] => 2 [2] => 7 [3] => 8 [4] => 10 [5] => 12 [6] => 13 [7] => 16 [8] => 20 )
 	// ※上の出力はあくまで例です。ご自分でわかりやすい出力に書き換えて下さっても構いません。
-	$numbers = array(20,10,2,12,7,16,8,13,1);
+	$numbers = range(0,20);
+	shuffle($numbers);
 
 	echo '【初期値】'.'<br>';
 	foreach ($numbers as $key) {
@@ -62,34 +63,60 @@
 	}
 	echo '<br>';
 	
-	function mergesort(&$numbers)
+	function mergesort(&$numbers)//引数は参照渡しで要素を変化させて返す
 	{
-		$last=count($numbers)-1;
-		$center=floor(count($numbers)/2);
-		$left=[];
+		$count=count($numbers);//配列の中身の数確認
+		$last=$count-1;//配列の最後の要素
+		$center=floor(count($numbers)/2);//配列の真ん中の要素
+		$left=[];//左と右にわけるために空の配列を用意
 		$right=[];
-		$result=[];
-		for ($i=0; $i <$center ; $i++) { 
-			$left[]=$numbers[$i];
+		$br='<br>';
+
+		for ($i=0; $i <$center ; $i++) { //左の要素の抽出
+			$left[]=$numbers[$i];//0から真ん中まで
 		}
-		for ($j=$center; $j <=$last ; $j++) { 
-			$right[]=$numbers[$j];
+		for ($j=$center; $j <=$last ; $j++) { //右の要素の抽出
+			$right[]=$numbers[$j];//真ん中からさいごまで
 		}
 
-		if (count($left)>1) {
+		if (count($left)>1) {//左の要素が１つ以上あるばあい再起処理
 			mergesort($left);
 		}
 
-		if (count($right)>1) {
+		if (count($right)>1) {//右の要素が１つ以上あるばあい再起処理
 			mergesort($right);
 		}
 		
-		echo '【交換前】'.'<br>';
-		echo var_dump($left).'左部分の２分割'.'<br>';
-		echo var_dump($right).'右部分の２分割'.'<br>';
+		echo "【交換前】{$br}";
+		echo var_dump($left)."左部分の２分割{$br}";
+		echo var_dump($right)."右部分の２分割{$br}";
+
+		$right_count=count($right);//右の配列の要素の数確認
+		$left_count=count($left);//左の配列の要素の数確認
+		$lpoint=0;//左の最初
+		$rpoint=0;//右の最初
+		$result=[];//下の条件の結果を入れるための空の配列
+
+		while ($lpoint<$left_count&&$rpoint<$right_count) {//右と左で要素の数がなくならない限り以下を実行
+			if ($left[$lpoint]<$right[$rpoint]) {//左のほうが小さい
+				$result[]=$left[$lpoint];//結果に左の要素を入れる
+				$lpoint++;//左の要素は次の要素に進む
+			}else{
+				$result[]=$right[$rpoint];//結果に右の要素を入れる
+				$rpoint++;//右の要素は次の要素に進む
+			}
+		}
+		while ($lpoint<$left_count) {//右の要素がなくなり左だけ残っていた場合
+			$result[]=$left[$lpoint];//残りの要素を結果に入れる
+			$lpoint++;//隣に移る
+		}
+		while ($rpoint<$right_count) {//左の要素がなくなり右だけ残っていた場合
+			$result[]=$right[$rpoint];//残りの要素をすべて結果に入れる
+			$rpoint++;//隣に移る
+		}
 
 		 echo '【マージ後】'.'<br>';
-		 $numbers=array_merge($left,$right);
+		 $numbers=$result;//上書き
 		 echo var_dump($numbers).'<br>';
 		}
 
