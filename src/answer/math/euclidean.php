602d73ddcd4cbb09db6382bcfb5987af7ff9badc
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-06 15:17:49 +0900

    euclidean　修正

--- src/answer/math/euclidean.php
+++ src/answer/math/euclidean.php
@@ -12,7 +12,9 @@
     // 2つの引数を取りその最大公約数を返すメソッドeuclidean を作成して返り値をechoして下さい。
     $num1=rand(1,1000);
     $num2=rand(1,1000);
+    $br='<br>';
 
+    echo "引数は【{$num1}】と【{$num2}】です。{$br}";
     euclidean($num1,$num2);
 
     function euclidean($num1,$num2)
@@ -23,7 +25,7 @@
         $num2=$remainder;
         $remainder=$num1%$num2;
       }
-      echo $num2;
+      echo '最大公約数は'.$num2;
 
     }
     ?>
