fc88784d70cdf160ffca4a11e0e8407a234e5f0c
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-07 13:15:45 +0900

    quicksort コメント記述

--- src/answer/sort/quicksort.php
+++ src/answer/sort/quicksort.php
@@ -27,30 +27,30 @@
   echo '<br>';
   quickSort($a,0,$a_last);//引数は配列a、配列の最初の要素、配列の最後の要素
 
-  function quickSort(&$a,$first,$last)//
+  function quickSort(&$a,$first,$last)//引数は配列、配列の最初の要素、配列の最後の要素
   {
-    $firstpoint=$first;//左から始まる要素
-    $lastpoint=$last;//右から始まる要素
+    $firstpoint=$first;//左から始まるポインター
+    $lastpoint=$last;//右から始まるポインター
     $pivod=$a[$firstpoint];//基準値は調べ始めの要素
 
-    if ($firstpoint>=$lastpoint) {//左と右が重なったらソート終了
+    if ($firstpoint>=$lastpoint) {//すべての要素がソートが終了したら終了
       return;
     }
 
     while ($firstpoint<=$lastpoint) {//左から右に探索して重なるまで続ける
-      while ($a[$firstpoint]<$pivod) {//真ん中と左を比べて、左が小さいなら一つ右に行く
+      while ($a[$firstpoint]<$pivod) {//真ん中の値と左の値を比べて、左が小さいなら一つ右に行く
           $firstpoint++;
       }
 
-      while ($pivod<$a[$lastpoint]) {//真ん中と右を比べて、右が小さいなら一つ左に行く
+      while ($pivod<$a[$lastpoint]) {//真ん中の値と右の値を比べて、右が小さいなら一つ左に行く
           $lastpoint--;
       }
 
 
-      if ($firstpoint>=$lastpoint) {//左と右が重なったらこの中での処理は終了
-      break;
+      if ($firstpoint>=$lastpoint) {//左ポインターと右ポインターが重なったらこの中での処理は終了
+          break;
       }
-      echo "左の現在地は【{$firstpoint}】、右の現在地は【{$lastpoint}】".'<br>';
+      echo "左ポインターの現在地は【{$firstpoint}】、右ポインターの現在地は【{$lastpoint}】".'<br>';
       echo'基準値は【'.$pivod.'】、【'. $a[$firstpoint].'】と【'.$a[$lastpoint].'】を交換します'.'<br>';
       $tmp=$a[$firstpoint];//以下、左の要素と右の要素の入れ替え
       $a[$firstpoint]=$a[$lastpoint];
@@ -58,14 +58,15 @@
       $firstpoint++;//次の要素を確認するために左と右どちらも進む
       $lastpoint--;
 
+      echo '【交換後の要素】'.'<br>';
       foreach ($a as $key) {
         echo $key.'&nbsp';
       }
       echo '<br>';
     }
 
-      quickSort($a, $first , $firstpoint-1);//左の要素、左端の要素、左のポインターが進んだ
-      quickSort($a, $lastpoint+1,$last);//右の要素、右の要素の始まり、右端の要素
+      quickSort($a, $first , $firstpoint-1);//左の要素、左端の要素、終わりはソートが終わった隣の要素まで
+      quickSort($a, $lastpoint+1,$last);//右の要素、右の要素の始まりはソートが終わった隣の値、右端の要素
   }
 
   ?>
