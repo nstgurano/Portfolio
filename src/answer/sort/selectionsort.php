1c86b69c54b1c37c12344d82a25d5a7c3179a53f
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-07 10:39:48 +0900

    コメント記述、ソースも若干修正

--- src/answer/sort/selectionsort.php
+++ src/answer/sort/selectionsort.php
@@ -16,7 +16,7 @@
   // 1 2 10 4 3
   // 上のように実行の過程も表示してください。
   $a = array(10,3,1,4,2);
-  $b=count($a);
+  $b=count($a);//配列の要素数確認
 
   selectionSort($a,$b);
   
@@ -28,41 +28,40 @@
     }
     echo '<br>';
 
-    for ($i=0; $i < $b-1; $i++) {
-      $min= $a[$i];
-      $key=$i;
+    for ($i=0; $i <=$b-1; $i++) {//線形探索で0番目から最後の配列まで調べる
+      $min= $a[$i];//最小の初期値
+      $key=$i;//最小値のキー
 
-      for ($x=$i+1; $x <$b ; $x++) {
-        if ($min > $a[$x]) {
-          $min=$a[$x];
-          $key=$x;
+      for ($x=$i+1; $x <$b ; $x++) {//最小値の隣の要素から最後の一つ手前までを調べる
+        if ($min > $a[$x]) {//出てきた数値が最小値よりも小さい場合
+          $min=$a[$x];//最小値を更新
+          $key=$x;//最小値のキーも更新
         }
       }
+      
       echo '<br>';
-      if ($key===$i) {
+      if ($key===$i) {//調べている数値が最小値であったら処理はスキップされる
         continue;
       }
 
-      $tmp=$a[$i];
+      echo $a[$i].'と'.$a[$key].'を交換します'.'<br>';
+      $tmp=$a[$i];//最小値と調べている数値の交換
       $a[$i]=$a[$key];
       $a[$key]=$tmp;
       
-      echo $a[$i].'と'.$a[$key].'を交換します';
+
+      echo '【交換後】'.'<br>';
+        foreach ($a as $key) {
+          echo $key.'&nbsp';
+        }
       echo '<br>';
-      foreach ($a as $key) {
-        echo $key.'&nbsp';
-      }
-      echo '<br>';
- 
     }
 
-    echo '<br>';
     echo '【ソート終了】';
     echo '<br>';
     foreach ($a as $key) {
       echo $key.'&nbsp';
     }
-    echo'<br>';
   }
   ?>
 
