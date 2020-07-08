10cbfc937df8da0c9585e501263a79d307b3c8fe
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-08 16:53:08 +0900

    breadth-first-search　成功　コメント記述あり

--- src/answer/graphsearch/breadth-first-search.php
+++ src/answer/graphsearch/breadth-first-search.php
@@ -28,9 +28,74 @@
   
   上のように、迷路データの通ってきた道を 0 → 1 へと変換して出力してください。
  */
-  $filename='src\answer\graphsearch\maze.csv';
-  $fileOutput=trim(fgets($filename));
-  echo "2";
+$maze=[];//迷路用の空の配列を用意
+$br='<br>';//改行
+$filename='maze.csv';//ファイル名
+$maze_data=file_get_contents($filename);//ファイルの中身をすべて取り込む
+$maze_data=explode("\r\n",$maze_data);//改行は取り除き、改行ごとに配列を作る
+foreach ($maze_data as $key) {
+  $maze[]=explode(',',$key);//','を取り除き、','を基準に配列を作る
+}
+$y_count=count($maze);//縦軸がどこまであるか確認
+$x_count=count($maze[0]);//横軸がどこまであるか確認※１行目だけ確認
+
+//////////////スタートの位置探し
+$start=start_search($maze,$y_count,$x_count);
+echo '【初期値迷路】'.$br;
+foreach ($maze_data as $maze_value) {
+  echo $maze_value.$br;
+}
+echo "スタートは【{$start[0]},{$start[1]}】です".$br;
+
+
+////////////ゴール探し
+$y=$start[0];//縦の現在地
+$x=$start[1];//横の現在地
+search_goal($maze,$y,$x);
+
+
+
+function start_search ($maze,$y_count,$x_count){//
+
+  $start_xy=[];//現在地確認の配列
+  for ($y=0; $y <$y_count ; $y++) { //スタートの縦・横位置を確認
+    for ($x=0; $x <$x_count ; $x++) {
+      if($maze[$y][$x]==='S'){//S発見したら、現在地に値を入れて返す
+        return $start_xy=[$y,$x];
+      }
+    }
+  }
+}
+
+
+function search_goal(&$maze,$y,$x){//
+
+  $y_count=count($maze);//縦軸がどこまであるか確認
+  $x_count=count($maze[0]);//横軸がどこまであるか確認※１行目だけ確認
+
+
+
+  if ($maze[$y][$x]==='X'||$x_count-1<$x||$x<0||$y_count-1<$y||$y<0||$maze[$y][$x]==='1') {//進めない条件、壁・１があるとき、壁が横・縦を超えないとき、
+    return;
+  }
+
+  if ($maze[$y][$x]==='0') {//進んだところは０→１に変える
+    $maze[$y][$x]='1';
+  }
+  if ($maze[$y][$x]==='G') {//ゴールの場合、終了
+    return;
+  }
+
+  echo "現在地は【{$y},{$x}】です".'<br>';
+  foreach ($maze as $maze_value) {//配列から迷路を再度作成
+    echo implode(',',$maze_value).'<br>';
+  }
+  search_goal($maze,$y,$x+1);//右に進む
+  search_goal($maze,$y,$x-1);//左に進む
+  search_goal($maze,$y-1,$x);//上に進む
+  search_goal($maze,$y+1,$x);//下に進む
+}
+
 ?>
 
 </body>
