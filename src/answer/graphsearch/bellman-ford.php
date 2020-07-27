c2d952cb9447a33427bc0c6e60d4d58afba81969
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-07-27 19:00:16 +0900

    ベルマンフォード修正完了

--- src/answer/graphsearch/bellman-ford.php
+++ src/answer/graphsearch/bellman-ford.php
@@ -22,44 +22,42 @@
  # 辺の配列（起点、終点、コストの配列）
 $edges = [
   [0, 1, 4], [0, 2, 3], [1, 2, 1], [1, 3, 1],
-  [1, 4, 5], [2, 5, 2], [4, 6, 2], [5, 4, 1],
+  [1, 4, 5], [2, 5, 2], [4, 6, 2], [5, 4, -1],
   [5, 6, 4]
 ];
-
 bellman_ford($edges, 7);
 function bellman_ford($edges, $num_v){
 
   $total_cost=[];
 
-  for ($i=0; $i < $num_v; $i++) { //頂点の数-1分の配列を用意
+  for ($i=0; $i < $num_v; $i++) { //頂点の数-1の配列を用意
     $total_cost[$i]=INF;//各頂点の初期値は無限大
   }
+
   $total_cost[0]=0;//始点のコストの初期化
   $from_num='';//始点
   $to_num='';//終点
   $cost_num='';//始点から終点へのコスト
-  $flg='';
 
-while($flg===true)
-  $flg=false;
-  for ($i=0; $i <$num_v ; $i++) { //頂点の数分確認
-    for ($j=0; $j < count($edges); $j++) { //辺の数だけ確認
-      $from_num=$edges[$j][0];//辺の始点の情報代入
-      $to_num=$edges[$j][1];//辺の終点の情報代入
-      $cost_num=$edges[$j][2];//辺のコストの情報代入
-
+  for ($j=0; $j <$num_v-1; $j++) { //頂点の数-1で負の閉路を検出できる
+    $flg=FALSE;//フラグの初期化
+    for ($k=0; $k < count($edges); $k++) { //辺の数だけ確認
+      $from_num=$edges[$k][0];//辺の始点の情報代入
+      $to_num=$edges[$k][1];//辺の終点の情報代入
+      $cost_num=$edges[$k][2];//辺のコストの情報代入
       if ($total_cost[$from_num]!==INF&&$total_cost[$to_num]>$total_cost[$from_num]+$cost_num) {//コストの初期値が更新されている場合かつ、始点よりも終点のコストのほうが大きい場合
         echo  "頂点【".$from_num."】のコスト【".$total_cost[$to_num]."】に【".$total_cost[$from_num]."】と【".$cost_num."】を足して更新".'<br>';
         $total_cost[$to_num]=$total_cost[$from_num]+$cost_num;//出発地点のコストを終点のコストに更新
-        $flg=TRUE;//更新があったら
-        // if ($i===$num_v-1) {//頂点-1だけループを回したら検索終了、負の閉路も検出し更新され続けたらループを止める
-        //   return "負の閉路を発見";
-        // }
+        $flg=TRUE;//更新がかかったらTRUEで
       }
     }
+    if ($flg===FALSE) {//コストの更新がかからなかった場合、すべてのノードのコストが更新された
+      var_dump($total_cost);
+      return;//処理は終了
+    }
   }
-  echo "〈始点【0】から各頂点への最短経路〉<br>";
-  var_dump($total_cost);
+    echo '負の閉路発見';//頂点-1ぶんのループを回していた場合、負の閉路が発生
+    return;
 }
 
 
