af99a058231e53b6064cec6338fd150e000f36bb
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-08-06 17:35:24 +0900

    P36まで終了

---
+++ src/SQL/sql_1.sql
@@ -0,0 +1,9 @@
+select CustomerName from Customers;
+select ProductName from Products;
+select Price from Products;
+select EmployeeName from Employees;
+select CustomerName , Address from Customers;
+select ProductID,ProductName,Price from Products;
+select EmployeeName,Email ,Height from Employees;
+select CustomerCode, CustomerName, CustomerCode from Customers;
+select ProductCode, Price, ProductName,ProductCode from Products;
