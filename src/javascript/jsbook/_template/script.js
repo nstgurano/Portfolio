7651f5e558160878ee4513d5433d24ddfd4fdd67
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-08-21 15:34:36 +0900

    自前の本

--- src/javascript/jsbook/_template/script.js
+++ src/javascript/jsbook/_template/script.js
@@ -43,6 +43,74 @@ for (const menu of array) {
     const li = `<li>${menu}</li>`;
     document.getElementById('output2').insertAdjacentHTML('beforeend',li);
 }
-array.forEach(element => {
-    
+
+const obj = {
+    'title': 'java',
+    'name': 'urano',
+    'price':300
+};
+for (const i in obj) {
+    console.log(i);
+}
+
+// function time(value) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(value*2)
+//         }, 5000);
+//     })
+// }
+// async function sample() {
+//     const t = await time(5);
+//     return t + 5;
+// }
+// sample().then(t => {
+//     console.log(t);
+// })
+// function sampleResolve(value) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(value);
+//         }, 1000);
+//     })
+// }
+
+// async function sample() {
+//     return await sampleResolve(5) * await sampleResolve(10) + await sampleResolve(20);
+// }
+// async function sample2() {
+//     const a = await sampleResolve(5);
+//     const b = await sampleResolve(10);
+//     const c = await sampleResolve(20);
+//     return a * b + c;
+// }
+// sample().then((value) => {
+//     console.log(value);
+// })
+// sample2().then((value) => {
+//     console.log(value);
+// })
+function sampleResolve(value) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(value);
+        }, 1000);
+    })
+}
+async function sample() {
+    for (let i = 0; i < 5; i++) {
+        const result = await sampleResolve(i);
+        console.log(i);
+    }
+    return `ループ終了`;
+}
+sample().then(value => {
+    console.log(value);
 });
+
+document.getElementById('form').onsubmit = function () {
+    event.preventDefault();
+    let search = document.getElementById('form').name.value;
+    document.getElementById('output3').textContent = `検索された文字は${search}`;
+}
+
---
+++ src/javascript/jsbook/_template/script2.js
@@ -0,0 +1,68 @@
+'use strict';
+// function countDown(due) {
+//     const now = new Date();
+//     const rest = due.getTime() - now.getTime();
+//     const sec = Math.floor(rest/1000)%60;
+//     const min = Math.floor(rest/1000/60)%60;
+//     const hours = Math.floor(rest/1000/60/60)%24;
+//     const days = Math.floor(rest / 1000 / 60 / 60 / 24);
+//     const count = [days, hours, min, sec]
+    
+//     return count;
+// }
+// let goal = new Date();
+// goal.setHours(16);
+// goal.setMinutes(59);
+// goal.setSeconds(59);
+
+// function recalc() {
+//     const counter = countDown(goal);
+//     const time = `${counter[1]}時間${counter[2]}分${counter[3]}秒`;
+//     document.getElementById('countdown').textContent = time;
+//     refresh();
+// }
+// function refresh() {
+//     setTimeout(recalc,1000)
+// }
+// recalc();
+
+// document.getElementById('form2').select.onchange = function () {
+//     location.href = document.getElementById('form2').select.value;
+// }
+
+// const agree = Cookies.get('cookie-agree');
+// const panel = document.getElementById('privacy-panel');
+// if (agree === 'yes') {
+//     document.body.removeChild(panel);
+// }else {
+//     document.getElementById('agreebtn').onclick = function () {
+//         Cookies.set('cookie-agree', 'yes'), { expires: 7 };
+//         document.body.removeChild(panel);
+//     };
+// }
+// document.getElementById('testbtn').onclick = function () {
+//     Cookies.remove('cookie-agree');
+// };
+
+
+$(document).ready(function () {
+    $.ajax({ url: 'data.json', dataType: 'json' })
+        .done(function (data) {
+            data.forEach(function(item,index) {
+                if (item.crowed==='yes') {
+                    const idName = '#' + item.id;
+                    $(idName).find('check').addClass('crowded');
+                }
+            });
+        })
+        .fail(function () {
+            window.alert('読み込みエラー');
+        });
+    $('click').on('click', function () {
+        if ($(this).hasClass('crowded')) {
+            $(this).text('残席わずか').addClass('red');
+        } else {
+            $(this).text('お席あります').addClass('green')
+        }
+    });
+});
