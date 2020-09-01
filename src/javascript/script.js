30f3bab66c0e94eea7f727699963121c2cb36569
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-01 16:27:04 +0900

    Dom操作

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,2 +1,7 @@
 'use strict';
 console.log('hello');
+const form2=`<h1>書き変え</h1>`;
+
+document.getElementById('form').onsubmit=function () {
+    document.getElementById('content').innerHTML=form2;
+}
