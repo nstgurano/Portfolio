5df8203c28ac8964d427ef20e0a1090fdd9d1afc
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-02 16:04:23 +0900

    あ

---
+++ src/PHP/practice/header.php
@@ -0,0 +1,14 @@
+<header class="page-header wrapper">
+    <h1><a href="home.php"><img src="./../../images/logo.svg" alt="ロゴ" class="logo"></a></h1>
+        <nav class="main-navi">
+            <ul class="list">
+                <li><a href="news.php">news</a></li>
+                <li><a href="#">menu</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </nav>
+        <div id="open_navi" class="hamburger">
+            <img src="hamburger.png" alt="">
+        </div>
+</header>
+<?php include('hamburger.php')?>
